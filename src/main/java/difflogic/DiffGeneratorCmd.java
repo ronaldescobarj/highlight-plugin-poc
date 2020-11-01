99bf56a904c822836201726282aeb81e0d3b484e
Ronald Escobar <ronaldescobarj@gmail.com>
2020-10-31 20:44:41 -0400

    fixed diff generation to use the simple files

--- src/main/java/difflogic/DiffGeneratorCmd.java
+++ src/main/java/difflogic/DiffGeneratorCmd.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 public class DiffGeneratorCmd implements IDiffGenerator {
     public boolean generateDiff() {
         boolean response = true;
-        final ProcessBuilder pBuilder = new ProcessBuilder("cmd.exe", "/c", "java -jar \"C:\\Users\\Dell\\Documents\\UCB\\10mo Semestre\\Seminario de Grado\\pluginPoC\\libs\\at.aau.softwaredynamics.runner-1.0-SNAPSHOT-jar-with-dependencies.jar\" -src \"C:\\Users\\Dell\\Documents\\UCB\\10mo Semestre\\Seminario de Grado\\diffTool\\IJM\\Test_old.java\" -dst \"C:\\Users\\Dell\\Documents\\UCB\\10mo Semestre\\Seminario de Grado\\diffTool\\IJM\\Test.java\" -c None -m IJM -w FS -g OTG");
+        //final ProcessBuilder pBuilder = new ProcessBuilder("cmd.exe", "/c", "java -jar \"C:\\Users\\Dell\\Documents\\UCB\\10mo Semestre\\Seminario de Grado\\pluginPoC\\libs\\at.aau.softwaredynamics.runner-1.0-SNAPSHOT-jar-with-dependencies.jar\" -src \"C:\\Users\\Dell\\Documents\\UCB\\10mo Semestre\\Seminario de Grado\\diffTool\\IJM\\Test_old.java\" -dst \"C:\\Users\\Dell\\Documents\\UCB\\10mo Semestre\\Seminario de Grado\\diffTool\\IJM\\Test.java\" -c None -m IJM -w FS -g OTG");
+        final ProcessBuilder pBuilder = new ProcessBuilder("cmd.exe", "/c", "java -jar \"C:\\Users\\Dell\\Documents\\UCB\\10mo Semestre\\Seminario de Grado\\pluginPoC\\libs\\at.aau.softwaredynamics.runner-1.0-SNAPSHOT-jar-with-dependencies.jar\" -src C:\\Users\\Dell\\IdeaProjects\\highlightTest\\src\\com\\company\\test_old.simple -dst C:\\Users\\Dell\\IdeaProjects\\highlightTest\\src\\com\\company\\test.simple -c None -m IJM -w FS -g OTG");
         pBuilder.directory(new File("C:\\Users\\Dell\\Documents\\outputs"));
         try {
             final Process process = pBuilder.start();
