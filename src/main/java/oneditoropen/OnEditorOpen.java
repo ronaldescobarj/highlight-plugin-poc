bbdc8c85fbc8cc5b83fcd335c7d6965a17ddbe76
Ronald Escobar <ronaldescobarj@gmail.com>
2020-11-14 22:18:47 -0400

    added logic to generate the diff not with the github api, but with the local commits

--- src/main/java/oneditoropen/OnEditorOpen.java
+++ src/main/java/oneditoropen/OnEditorOpen.java
@@ -7,6 +7,7 @@ import at.aau.softwaredynamics.gen.NodeType;
 import at.aau.softwaredynamics.gen.OptimizedJdtTreeGenerator;
 import at.aau.softwaredynamics.matchers.JavaMatchers;
 import at.aau.softwaredynamics.runner.util.ClassifierFactory;
+import at.aau.softwaredynamics.runner.util.GitHelper;
 import com.github.gumtreediff.gen.TreeGenerator;
 import com.github.gumtreediff.matchers.Matcher;
 import com.intellij.openapi.editor.Document;
@@ -16,14 +17,24 @@ import com.intellij.openapi.editor.event.EditorFactoryListener;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.diff.Diff;
 import gitremote.GitRemote;
 import models.DiffRow;
 import models.GitCommit;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.treewalk.filter.PathSuffixFilter;
+import org.eclipse.jgit.util.io.NullOutputStream;
 import org.jetbrains.annotations.NotNull;
 import services.EditorService;
 import services.GitService;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class OnEditorOpen implements EditorFactoryListener {
@@ -37,6 +48,38 @@ public class OnEditorOpen implements EditorFactoryListener {
         ArrayList<DiffRow> diffs = getDiff(changes);
         EditorService editorService = editor.getProject().getService(EditorService.class);
         editorService.setDiffsOfLastOpenedEditor(diffs);
+
+//        repoTest(editor.getProject().getBasePath());
+    }
+
+    public void repoTest(String path) {
+        try{
+            Repository repository = GitHelper.openRepository(path);
+            Collection<RevCommit> commits = GitHelper.getCommits(repository, "HEAD");
+            for (RevCommit commit : commits) {
+                RevCommit[] parents = commit.getParents();
+                DiffFormatter df = new DiffFormatter(NullOutputStream.INSTANCE);
+                df.setRepository(repository);
+                df.setPathFilter(PathSuffixFilter.create("test.simple"));
+                df.setDetectRenames(true);
+                try {
+                    if (parents.length > 0) {
+                        List<DiffEntry> diffs = df.scan(parents[0], commit.getTree());
+                        System.out.println("funciono");
+                        for (DiffEntry diff : diffs) {
+                            String oldCode = GitHelper.getFileContent(diff.getOldId(), repository);
+                            String newCode = GitHelper.getFileContent(diff.getNewId(), repository);
+                            System.out.println("test");
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+            }
+            System.out.println("entro");
+        } catch(Exception e) {
+            System.out.println("fallo");
+        }
     }
 
     private ArrayList<DiffRow> getDiff(List<SourceCodeChange> changes) {
@@ -62,13 +105,50 @@ public class OnEditorOpen implements EditorFactoryListener {
         return document.getText();
     }
 
+//    private String getPreviousCommitFileContent(Editor editor) {
+//        String relativePath = getRelativePath(editor);
+//        String githubApiUrl = getGithubApiUrl(editor);
+//        GitRemote gitRemote = new GitRemote();
+//        GitCommit[] commits = gitRemote.getCommits(githubApiUrl);
+//        String previousCommitSha = commits[1].getSha();
+//        return gitRemote.getPreviousCommitFileContent(githubApiUrl, previousCommitSha, relativePath);
+//    }
+
     private String getPreviousCommitFileContent(Editor editor) {
-        String relativePath = getRelativePath(editor);
-        String githubApiUrl = getGithubApiUrl(editor);
-        GitRemote gitRemote = new GitRemote();
-        GitCommit[] commits = gitRemote.getCommits(githubApiUrl);
-        String previousCommitSha = commits[1].getSha();
-        return gitRemote.getPreviousCommitFileContent(githubApiUrl, previousCommitSha, relativePath);
+        String fileName = getFileName(editor);
+        try {
+            Repository repository = GitHelper.openRepository(editor.getProject().getBasePath());
+            Collection<RevCommit> commits = GitHelper.getCommits(repository, "HEAD");
+            RevCommit commit = commits.iterator().next();
+            DiffFormatter diffFormatter = createDiffFormatter(repository, fileName);
+            return getPreviousCommitContent(diffFormatter, commit, repository);
+        } catch(Exception e) {
+            System.out.println("fallo");
+            return "";
+        }
+    }
+
+    private String getPreviousCommitContent(DiffFormatter diffFormatter, RevCommit commit, Repository repository) {
+        RevCommit[] parents = commit.getParents();
+        try {
+            if (parents.length != 0) {
+                List<DiffEntry> diffs = diffFormatter.scan(parents[0], commit.getTree());
+                DiffEntry diff = diffs.get(0);
+                return GitHelper.getFileContent(diff.getOldId(), repository);
+            }
+            return "";
+        } catch(IOException e) {
+            System.out.println("fallo2");
+            return "";
+        }
+    }
+
+    private DiffFormatter createDiffFormatter(Repository repository, String fileName) {
+        DiffFormatter diffFormatter = new DiffFormatter(NullOutputStream.INSTANCE);
+        diffFormatter.setRepository(repository);
+        diffFormatter.setPathFilter(PathSuffixFilter.create(fileName));
+        diffFormatter.setDetectRenames(true);
+        return diffFormatter;
     }
 
     private String getRelativePath(Editor editor) {
@@ -77,6 +157,12 @@ public class OnEditorOpen implements EditorFactoryListener {
         Project project = editor.getProject();
         String projectPath = project.getBasePath();
         return filePath.replaceAll(projectPath, "");
+    }
+
+    private String getFileName(Editor editor) {
+        Document document = editor.getDocument();
+        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        return virtualFile.getName();
     }
 
     private String getGithubApiUrl(Editor editor) {
--- src/main/java/onopenproject/OnOpenProject.java
+++ src/main/java/onopenproject/OnOpenProject.java
@@ -1,11 +1,16 @@
 package onopenproject;
 
+import at.aau.softwaredynamics.runner.util.GitHelper;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
 import gitremote.GitRemote;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.jetbrains.annotations.NotNull;
 import services.GitService;
+
+import java.util.Collection;
 
 public class OnOpenProject implements ProjectManagerListener {
 
