22357bfa13fd72b70be624e500d6924bdcacb723
Ronald Escobar <ronaldescobarj@gmail.com>
2021-01-13 19:55:13 -0400

    added javalanguage files

---
+++ src/main/java/javalanguage/JavaIcons.java
@@ -0,0 +1,9 @@
+package javalanguage;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class JavaIcons {
+    public static final Icon FILE = IconLoader.getIcon("/images/mygear.png");
+}
