085521114926a04b5235020d10397e757c87bacd
ronaldescobarj <ronaldescobarj@gmail.com>
2021-03-29 23:38:36 -0400

    added logic to display commits on select commit menu

--- src/main/java/menus/SelectCommitAction.java
+++ src/main/java/menus/SelectCommitAction.java
@@ -2,7 +2,15 @@ package menus;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import git.GitLocal;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.jetbrains.annotations.NotNull;
+import services.GitService;
+
+import java.util.List;
 
 public class SelectCommitAction extends AnAction {
     @Override
@@ -12,6 +20,17 @@ public class SelectCommitAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        // Using the event, implement an action. For example, create and show a dialog.
+        Project currentProject = e.getProject();
+        GitService gitService = currentProject.getService(GitService.class);
+        Repository repository = gitService.getRepository();
+        GitLocal gitLocal = new GitLocal(repository);
+        List<RevCommit> commits = gitLocal.getSelectedLatestCommitsDescendant(5);
+        StringBuilder content = new StringBuilder("");
+        for (RevCommit commit: commits) {
+            content.append("SHA: ").append(commit.getName()).append("\n");
+            content.append("Author: ").append(commit.getAuthorIdent().getEmailAddress()).append("\n");
+            content.append("Date: ").append(commit.getAuthorIdent().getWhen().toString()).append("\n\n");
+        }
+        Messages.showMessageDialog(currentProject, content.toString(), "Select commit", Messages.getInformationIcon());
     }
 }
